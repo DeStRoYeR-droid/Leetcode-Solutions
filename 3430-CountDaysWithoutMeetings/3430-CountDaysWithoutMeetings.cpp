2d8f2e4af3387d6c0694d0bf2cedef925cf171de
Immanuel Vivek <63895010+DeStRoYeR-droid@users.noreply.github.com>
2026-07-06 18:53:03 +0530

    Create 3430-CountDaysWithoutMeetings

---
+++ 3430-CountDaysWithoutMeetings/3430-CountDaysWithoutMeetings.cpp
@@ -0,0 +1,17 @@
+// Last updated: 06/07/2026, 18:53:03
+class Solution {
+public:
+    int countDays(int days, vector<vector<int>>& meetings) {
+        sort(meetings.begin(), meetings.end());
+        int prevEnd = 0;
+
+        for (auto& meeting : meetings){
+            int start = max(meeting[0], prevEnd + 1);
+            int length = meeting[1] - start + 1;
+            days -= max(length, 0);
+            prevEnd = max(prevEnd, meeting[1]);
+        }
+
+        return days;
+    }
+};
