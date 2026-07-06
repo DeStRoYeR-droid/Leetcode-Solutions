57907315d65175ca1513e3203179188cddf4394b
Immanuel Vivek <63895010+DeStRoYeR-droid@users.noreply.github.com>
2026-07-06 18:58:56 +0530

    Create 2199-TwoFurthestHousesWithDifferentColors

---
+++ 2199-TwoFurthestHousesWithDifferentColors/2199-TwoFurthestHousesWithDifferentColors.cpp
@@ -0,0 +1,15 @@
+// Last updated: 06/07/2026, 18:58:56
+class Solution {
+public:
+    int maxDistance(vector<int>& colors) {
+        const int n=colors.size();
+        int c0=colors[0], cN=colors[n-1];
+        int lMax=0, rMax=0;
+        for(int i=0; i<n; i++){
+            int c=colors[i];
+            if (c0!=c) lMax=max(lMax, i);
+            if (cN!=c) rMax=max(rMax, n-1-i);
+        }
+        return max(lMax, rMax);
+    }
+};
