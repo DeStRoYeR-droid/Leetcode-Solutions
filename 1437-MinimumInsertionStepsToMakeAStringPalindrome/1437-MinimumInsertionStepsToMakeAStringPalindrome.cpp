7f8d5f71e90f39287c4929f3d87d6f43ec0ed5a2
Immanuel Vivek <63895010+DeStRoYeR-droid@users.noreply.github.com>
2026-07-06 19:04:14 +0530

    Create 1437-MinimumInsertionStepsToMakeAStringPalindrome

---
+++ 1437-MinimumInsertionStepsToMakeAStringPalindrome/1437-MinimumInsertionStepsToMakeAStringPalindrome.cpp
@@ -0,0 +1,24 @@
+// Last updated: 06/07/2026, 19:04:14
+class Solution {
+public:
+    int minInsertions(string s) {
+        string t = s;
+        reverse(s.begin(), s.end());
+        return s.length() - lcs(t, s);
+    }
+    int lcs(string& text1, string& text2){
+        const int length1 = text1.length();
+        const int length2 = text2.length();
+
+        vector<vector<int>>dp(length1 + 1, vector<int>(length2+1));
+        
+        for (int i = 0; i < length1; i++){
+            for (int j = 0; j < length2; j++){
+                dp[i+1][j+1] = (text1[i] == text2[j]) ? 1 + dp[i][j] : 
+                            max(dp[i][j+1], dp[i+1][j]);
+            }
+        }
+
+        return dp[length1][length2];
+    }
+};
