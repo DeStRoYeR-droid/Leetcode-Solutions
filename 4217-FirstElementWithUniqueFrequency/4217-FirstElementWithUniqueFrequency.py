61fff2faf78902e1b0d56fb1dde41ac7ee3b7613
Immanuel Vivek <63895010+DeStRoYeR-droid@users.noreply.github.com>
2026-07-06 18:47:20 +0530

    Create 4217-FirstElementWithUniqueFrequency

---
+++ 4217-FirstElementWithUniqueFrequency/4217-FirstElementWithUniqueFrequency.py
@@ -0,0 +1,12 @@
+# Last updated: 06/07/2026, 18:47:19
+from collections import Counter
+class Solution:
+    def firstUniqueFreq(self, nums: List[int]) -> int:
+        count = Counter(nums)
+        freq_count = Counter(count.values())
+        for num in nums:
+            if freq_count[count[num]] == 1:
+                return num
+
+        return -1
+__import__("atexit").register(lambda: open("display_runtime.txt", "w").write("0"))
