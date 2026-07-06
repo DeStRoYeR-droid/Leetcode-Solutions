6929fe646bebe5d2831a544cf9348c7ee57e824c
Immanuel Vivek <63895010+DeStRoYeR-droid@users.noreply.github.com>
2026-07-06 18:57:25 +0530

    Create 2398-CheckIfMatrixIsXMatrix

---
+++ 2398-CheckIfMatrixIsXMatrix/2398-CheckIfMatrixIsXMatrix.cpp
@@ -0,0 +1,16 @@
+// Last updated: 06/07/2026, 18:57:25
+class Solution {
+public:
+    bool checkXMatrix(vector<vector<int>>& grid) {
+        const int n = grid.size();
+        for (int i = 0; i < n; ++i){
+            for (int j = 0; j < n; ++j){
+                if ((i == j || (i + j) == n - 1)){
+                    if (grid[i][j] == 0) return false;
+                }
+                else if (grid[i][j]) return false;
+            }
+        }
+        return true;
+    }
+};
