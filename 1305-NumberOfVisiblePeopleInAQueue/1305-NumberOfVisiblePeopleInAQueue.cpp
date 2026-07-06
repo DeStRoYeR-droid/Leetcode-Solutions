f3c7267c0446a9ac425cfe79d3c72f96b71521bd
Immanuel Vivek <63895010+DeStRoYeR-droid@users.noreply.github.com>
2026-07-06 19:05:09 +0530

    Create 1305-NumberOfVisiblePeopleInAQueue

---
+++ 1305-NumberOfVisiblePeopleInAQueue/1305-NumberOfVisiblePeopleInAQueue.cpp
@@ -0,0 +1,19 @@
+// Last updated: 06/07/2026, 19:05:08
+class Solution {
+public:
+    vector<int> canSeePersonsCount(vector<int>& heights) {
+        int n = heights.size();
+        vector<int> ans(n);
+        stack<int> st;
+        for (int i = n - 1; i >= 0; --i) {
+            while (!st.empty() && heights[i] > st.top()) {
+                st.pop();
+                ++ans[i];
+            }
+            if (!st.empty())
+                ++ans[i];
+            st.push(heights[i]);
+        }
+        return ans;
+    }
+};
