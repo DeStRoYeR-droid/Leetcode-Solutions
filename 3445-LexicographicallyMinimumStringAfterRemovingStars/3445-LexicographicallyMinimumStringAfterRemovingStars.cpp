79585d371b84934845fc0dbec3708589cbc5c069
Immanuel Vivek <63895010+DeStRoYeR-droid@users.noreply.github.com>
2026-07-06 18:52:57 +0530

    Create 3445-LexicographicallyMinimumStringAfterRemovingStars

---
+++ 3445-LexicographicallyMinimumStringAfterRemovingStars/3445-LexicographicallyMinimumStringAfterRemovingStars.cpp
@@ -0,0 +1,24 @@
+// Last updated: 06/07/2026, 18:52:57
+class Solution {
+public:
+    string clearStars(string s) {
+        vector<vector<int>> pos(26);
+        priority_queue<char, vector<char>, greater<char>> pq;
+
+        for (int i = 0; i < s.size(); ++i){
+            if (s[i] == '*'){
+                char c = pq.top();
+                s[pos[c - 'a'].back()] = '*';
+                pos[c - 'a'].pop_back();
+                if (pos[c - 'a'].empty()) pq.pop();
+            }
+            else {
+                if (pos[s[i] - 'a'].empty()) pq.push(s[i]);
+                pos[s[i] - 'a'].push_back(i);
+            }
+        }
+        string result;
+        for (const char& ch : s) if (ch >= 'a') result += ch;
+        return result;
+    }
+};
