61a2af8b303f8a811edb022078f2ddefe212e0e0
Immanuel Vivek <63895010+DeStRoYeR-droid@users.noreply.github.com>
2026-07-06 18:58:14 +0530

    Create 2292-CountingWordsWithAGivenPrefix

---
+++ 2292-CountingWordsWithAGivenPrefix/2292-CountingWordsWithAGivenPrefix.py
@@ -0,0 +1,9 @@
+# Last updated: 06/07/2026, 18:58:14
+class Solution:
+    def prefixCount(self, words: List[str], pref: str) -> int:
+        result = 0
+        for word in words:
+            if (word.startswith(pref)):
+                result += 1
+
+        return result
