0cfae0997ecc0b9c5000a72193288e1a805a1ced
Immanuel Vivek <63895010+DeStRoYeR-droid@users.noreply.github.com>
2026-07-06 18:49:46 +0530

    Create 3945-MinimumSensorsToCoverGrid

---
+++ 3945-MinimumSensorsToCoverGrid/3945-MinimumSensorsToCoverGrid.cpp
@@ -0,0 +1,10 @@
+// Last updated: 06/07/2026, 18:49:46
+class Solution {
+public:
+    int minSensors(int n, int m, int k) {
+        long long side=2*k+1;
+        long long x=(n+side-1)/side;
+        long long y=(m+side-1)/side;
+        return (int)x*y;
+    }
+};
