8a055541112589c71cfad18e74a9081aa5f28d57
Immanuel Vivek <63895010+DeStRoYeR-droid@users.noreply.github.com>
2026-07-06 18:56:04 +0530

    Create 2598-ShortestDistanceToTargetStringInACircularArray

---
+++ 2598-ShortestDistanceToTargetStringInACircularArray/2598-ShortestDistanceToTargetStringInACircularArray.cpp
@@ -0,0 +1,13 @@
+// Last updated: 06/07/2026, 18:56:04
+class Solution {
+public:
+    int closestTarget(vector<string>& words, string target, int start) {
+        int n = words.size();
+        for (int i = 0; i <= (n >> 1); i++)
+            if (words[(start + i) % n] == target |
+                words[(start - i + n) % n] == target)
+                return i;
+                
+        return -1;
+    }
+};
