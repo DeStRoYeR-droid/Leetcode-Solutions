f1caf7218c2bdc70dfbb8ef0704d082efe17f0a5
Immanuel Vivek <63895010+DeStRoYeR-droid@users.noreply.github.com>
2026-07-06 18:57:29 +0530

    Create 2392-SuccessfulPairsOfSpellsAndPotions

---
+++ 2392-SuccessfulPairsOfSpellsAndPotions/2392-SuccessfulPairsOfSpellsAndPotions.cpp
@@ -0,0 +1,28 @@
+// Last updated: 06/07/2026, 18:57:29
+class Solution {
+public:
+    vector<int> successfulPairs(vector<int>& spells, vector<int>& potions, long long success) {
+        const int n = spells.size();
+        const int m = potions.size();
+
+        vector<int> pairs(n, 0);
+        sort(potions.begin(), potions.end());
+        
+        for (int i = 0; i < n; i++) {
+            int spell = spells[i];
+            int left = 0;
+            int right = m - 1;
+            while (left <= right) {
+                int mid = left + (right - left) / 2;
+                long long product = (long long)spell * (long long)potions[mid];
+                if (product >= success) {
+                    right = mid - 1;
+                } else {
+                    left = mid + 1;
+                }
+            }
+            pairs[i] = m - left;
+        }
+        return pairs;
+    }
+};
