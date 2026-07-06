cdd4a0d8ea80f01444e2e976925f570a2d56ebf5
Immanuel Vivek <63895010+DeStRoYeR-droid@users.noreply.github.com>
2026-07-06 18:56:26 +0530

    Create 2533-BitwiseXorOfAllPairings

---
+++ 2533-BitwiseXorOfAllPairings/2533-BitwiseXorOfAllPairings.cpp
@@ -0,0 +1,14 @@
+// Last updated: 06/07/2026, 18:56:26
+class Solution {
+public:
+    int xorAllNums(vector<int>& nums1, vector<int>& nums2) {
+        int c1 = nums1.size();
+        int c2 = nums2.size();
+        int x1 = 0, x2 = 0;
+
+        if (c1 % 2) for (int num : nums2) x2 ^= num;
+        if (c2 % 2) for (int num : nums1) x1 ^= num;
+
+        return x1 ^ x2;
+    }
+};
