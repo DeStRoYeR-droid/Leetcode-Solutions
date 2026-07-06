fc27835b498e25ae895f3c330178a8e71b9eaebd
Immanuel Vivek <63895010+DeStRoYeR-droid@users.noreply.github.com>
2026-07-06 18:58:02 +0530

    Create 2308-DivideArrayIntoEqualPairs

---
+++ 2308-DivideArrayIntoEqualPairs/2308-DivideArrayIntoEqualPairs.cpp
@@ -0,0 +1,9 @@
+// Last updated: 06/07/2026, 18:58:02
+class Solution {
+public:
+    bool divideArray(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        for (int i = 0; i < nums.size(); i += 2) if (nums[i] != nums[i+1]) return false;
+        return true;
+    }
+};
