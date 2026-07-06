bc58e279d69cccde2480a59b7d3b5a6e89ef183d
Immanuel Vivek <63895010+DeStRoYeR-droid@users.noreply.github.com>
2026-07-06 18:56:38 +0530

    Create 2507-NumberOfCommonFactors

---
+++ 2507-NumberOfCommonFactors/2507-NumberOfCommonFactors.cpp
@@ -0,0 +1,14 @@
+// Last updated: 06/07/2026, 18:56:37
+class Solution {
+public:
+    int commonFactors(int a, int b) {
+        int minval = min(a, b);
+        int count = 0;
+        for(int i = 1; i <= minval; i++){
+            if(a % i == 0 && b % i == 0){
+                count++;
+            }
+        }
+        return count;
+    }
+};
