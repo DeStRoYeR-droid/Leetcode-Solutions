e1b411e054353929bed7231b3884bdbc514ed8a3
Immanuel Vivek <63895010+DeStRoYeR-droid@users.noreply.github.com>
2026-07-06 18:50:50 +0530

    Create 3774-CheckIfDigitsAreEqualInStringAfterOperationsIi

---
+++ 3774-CheckIfDigitsAreEqualInStringAfterOperationsIi/3774-CheckIfDigitsAreEqualInStringAfterOperationsIi.cpp
@@ -0,0 +1,68 @@
+// Last updated: 06/07/2026, 18:50:49
+class Solution {
+public:
+    bool hasSameDigits(string s) {
+        int L = s.size();
+        int m = L - 2;
+        int s1 = 0, s2 = 0;
+        for (int i = 0; i <= m; i++) {
+            int val = binMod10(m, i);
+            int d1 = s[i] - '0';
+            int d2 = s[i + 1] - '0';
+            s1 = (s1 + val * d1) % 10;
+            s2 = (s2 + val * d2) % 10;
+        }
+        return s1 == s2;
+    }
+
+    int binMod10(int n, int k){
+        int mod2 = binMod2(n, k);
+        int mod5 = binMod5(n, k);
+
+        for (int i = 0; i < 10; ++i)
+            if (i % 2 == mod2 && i % 5 == mod5)
+                return i;
+        
+        return 0;
+    }
+
+    int binMod5(int n, int k) {
+        int res = 1;
+        while (n > 0 || k > 0) {
+            int nd = n % 5;
+            int kd = k % 5;
+            if (kd > nd)
+                return 0;
+            res = (res * binsmall(nd, kd)) % 5;
+            n /= 5;
+            k /= 5;
+        }
+        return res;
+    }
+
+    int binMod2(int n, int k) {
+        while (k > 0) {
+            if ((k & 1) > (n & 1))
+                return 0;
+            n >>= 1;
+            k >>= 1;
+        }
+        return 1;
+    }
+
+    int binsmall(int n, int k) {
+        if (k > n)
+            return 0;
+        int fact[5] = {1, 1, 2, 1, 4};
+        int numerator = fact[n];
+        int denominator = (fact[k] * fact[n - k]) % 5;
+        int deninv = 0;
+        for (int i = 0; i < 5; i++) {
+            if ((denominator * i) % 5 == 1) {
+                deninv = i;
+                break;
+            }
+        }
+        return (numerator * deninv) % 5;
+    }
+};
