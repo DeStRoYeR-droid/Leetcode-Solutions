6cd3f58bad10243d1074bf441ddbd076395efe0c
Immanuel Vivek <63895010+DeStRoYeR-droid@users.noreply.github.com>
2026-07-06 18:58:10 +0530

    Create 2288-CountOperationsToObtainZero

---
+++ 2288-CountOperationsToObtainZero/2288-CountOperationsToObtainZero.cpp
@@ -0,0 +1,13 @@
+// Last updated: 06/07/2026, 18:58:09
+class Solution {
+public:
+    int countOperations(int num1, int num2) {
+        int result=0;
+        while(num1 != 0 && num2 != 0){
+            if(num1 >= num2) num1 = num1 - num2;
+            else num2 = num2 - num1; 
+            result++;
+        }
+        return result;
+    }
+};
