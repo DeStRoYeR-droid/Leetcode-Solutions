ade463a94be82055ee98db8a06dc9599e4849e1b
Immanuel Vivek <63895010+DeStRoYeR-droid@users.noreply.github.com>
2026-07-06 18:56:28 +0530

    Create 2529-RangeProductQueriesOfPowers

---
+++ 2529-RangeProductQueriesOfPowers/2529-RangeProductQueriesOfPowers.cpp
@@ -0,0 +1,24 @@
+// Last updated: 06/07/2026, 18:56:28
+class Solution {
+public:
+    vector<int> productQueries(int n, vector<vector<int>>& queries) {
+        vector<long> p2;
+        vector<int> res;
+        int mod = 1e9+7, i = 0;
+        while (n) {
+            if (n & 1) {
+                p2.push_back(1L << i);
+            }
+            n >>= 1;
+            i++;
+        }
+        for (auto &q : queries) {
+            long long prod = 1;
+            for (int j = q[0]; j <= q[1]; j++) {
+                prod = (prod * p2[j]) % mod;
+            }
+            res.push_back(prod);
+        }
+        return res;
+    }
+};
