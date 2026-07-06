e1c771ec39c3778d9d20972e35f58663602f15fe
Immanuel Vivek <63895010+DeStRoYeR-droid@users.noreply.github.com>
2026-07-06 18:52:16 +0530

    Create 3605-ConstructTheMinimumBitwiseArrayI

---
+++ 3605-ConstructTheMinimumBitwiseArrayI/3605-ConstructTheMinimumBitwiseArrayI.cpp
@@ -0,0 +1,12 @@
+// Last updated: 06/07/2026, 18:52:15
+class Solution {
+public:
+    vector<int> minBitwiseArray(vector<int>& nums) {
+        vector<int> result;
+        for(const int& n : nums) {
+            if(n != 2) result.push_back(n - ((n + 1) & (-n - 1)) / 2);
+            else result.push_back(-1);
+        }   
+        return result;
+    }
+};
