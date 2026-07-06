eb5bbf2628bc8ab26d494c509188359e79211c11
Immanuel Vivek <63895010+DeStRoYeR-droid@users.noreply.github.com>
2026-07-06 18:54:06 +0530

    Create 3226-MinimumNumberGame

---
+++ 3226-MinimumNumberGame/3226-MinimumNumberGame.cpp
@@ -0,0 +1,11 @@
+// Last updated: 06/07/2026, 18:54:06
+class Solution {
+public:
+    vector<int> numberGame(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        for (int i = 0; i < nums.size() - 1; i += 2){
+            swap(nums[i], nums[i + 1]);
+        }
+        return nums;
+    }
+};
