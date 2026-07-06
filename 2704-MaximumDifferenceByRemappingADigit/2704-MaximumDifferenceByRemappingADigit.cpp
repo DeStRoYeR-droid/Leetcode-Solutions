3bcf4849d676a5bd51dad7e0edcf189101c087e1
Immanuel Vivek <63895010+DeStRoYeR-droid@users.noreply.github.com>
2026-07-06 18:55:34 +0530

    Create 2704-MaximumDifferenceByRemappingADigit

---
+++ 2704-MaximumDifferenceByRemappingADigit/2704-MaximumDifferenceByRemappingADigit.cpp
@@ -0,0 +1,23 @@
+// Last updated: 06/07/2026, 18:55:33
+class Solution {
+public:
+    int minMaxDifference(int num) {
+        string s = to_string(num);
+        string maxStr = s, minStr = s;
+        for (char& c : s){
+            if (c != '9'){
+                for (char& ch : maxStr) if (ch == c) ch = '9';
+                break;
+            }
+        }
+
+        for (char& c : s){
+            if (c != 0){
+                for (char& ch : minStr) if (ch == c) ch = '0';
+                break;
+            }
+        }
+
+        return stoi(maxStr) - stoi(minStr);
+    }
+};
