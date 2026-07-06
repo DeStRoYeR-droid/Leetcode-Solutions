582d004b1775f0a99651a06290c411f7b8c18f8b
Immanuel Vivek <63895010+DeStRoYeR-droid@users.noreply.github.com>
2026-07-06 18:47:02 +0530

    Create 4275-TrafficSignalColor

---
+++ 4275-TrafficSignalColor/4275-TrafficSignalColor.cpp
@@ -0,0 +1,10 @@
+// Last updated: 06/07/2026, 18:47:02
+class Solution {
+public:
+    string trafficSignal(int timer) {
+        if (timer == 0) return "Green";
+        if (timer == 30) return "Orange";
+        if (30 < timer && timer <= 90) return "Red";
+        return "Invalid";
+    }
+};
