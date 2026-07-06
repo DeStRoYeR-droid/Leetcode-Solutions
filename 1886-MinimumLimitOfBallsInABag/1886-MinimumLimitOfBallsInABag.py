7613150565366558d0dbc3fae489d61838a9eccb
Immanuel Vivek <63895010+DeStRoYeR-droid@users.noreply.github.com>
2026-07-06 19:01:00 +0530

    Create 1886-MinimumLimitOfBallsInABag

---
+++ 1886-MinimumLimitOfBallsInABag/1886-MinimumLimitOfBallsInABag.py
@@ -0,0 +1,12 @@
+# Last updated: 06/07/2026, 19:00:59
+class Solution:
+    def minimumSize(self, nums: List[int], maxOperations: int) -> int:
+        low, high = 1, max(nums)
+        while (low < high):
+            mid = (low + high) // 2
+            if sum((n-1) // mid for n in nums) <= maxOperations:
+                high = mid
+            else:
+                low = mid + 1
+        
+        return high
