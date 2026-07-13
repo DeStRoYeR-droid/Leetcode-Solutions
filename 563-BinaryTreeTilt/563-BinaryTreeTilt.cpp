571a5b80538ea4dad4650e8192281bc9936e59ea
Immanuel Vivek <63895010+DeStRoYeR-droid@users.noreply.github.com>
2026-07-13 22:18:26 +0530

    Create 563-BinaryTreeTilt

---
+++ 563-BinaryTreeTilt/563-BinaryTreeTilt.cpp
@@ -0,0 +1,29 @@
+// Last updated: 13/07/2026, 22:18:24
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int findTilt(TreeNode* root) {
+        int result = 0;
+        traverser(root, result);
+        return result;
+    }
+    int traverser(TreeNode* root, int& value){
+        if (root == nullptr) return 0;
+        
+        const int leftSum = traverser(root->left, value);
+        const int rightSum = traverser(root->right, value);
+
+        value += abs(leftSum - rightSum);
+        return leftSum + rightSum + root->val;
+    }
+};
