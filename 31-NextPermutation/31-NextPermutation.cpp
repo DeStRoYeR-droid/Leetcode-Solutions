23499bf658008fb9a1782d06bf4c99fb8d8b722e
Immanuel Vivek <63895010+DeStRoYeR-droid@users.noreply.github.com>
2026-07-13 22:29:50 +0530

    Create 31-NextPermutation

---
+++ 31-NextPermutation/31-NextPermutation.cpp
@@ -0,0 +1,18 @@
+// Last updated: 13/07/2026, 22:29:48
+class Solution {
+public:
+    void nextPermutation(vector<int>& nums) {
+        const int n = nums.size();
+        int i = n - 2;
+
+        while (i >= 0 && nums[i] >= nums[i+1]) i--;
+
+        if (i >= 0){
+            int j = n - 1;
+            while (nums[j] <= nums[i]) j--;
+            swap(nums[i], nums[j]);
+        }
+
+        reverse(nums.begin() + i + 1, nums.end());
+    }
+};
