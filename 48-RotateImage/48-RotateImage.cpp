f4b1344e9c3eb93c20b6b579a292310f05bad55d
Immanuel Vivek <63895010+DeStRoYeR-droid@users.noreply.github.com>
2026-07-13 22:29:14 +0530

    Create 48-RotateImage

---
+++ 48-RotateImage/48-RotateImage.cpp
@@ -0,0 +1,18 @@
+// Last updated: 13/07/2026, 22:29:12
+class Solution {
+public:
+    void rotate(vector<vector<int>>& matrix) {
+        const int n = matrix.size();
+        
+        for (int i = 0; i < n; ++i){
+            for (int j = 0; j < n - i; ++j){
+                swap(matrix[i][j], matrix[n-j-1][n-i-1]);
+            }
+        }
+        for (int i = 0; i < n / 2; ++i){
+            for (int j = 0; j < n; ++j){
+                swap(matrix[i][j], matrix[n-i-1][j]);
+            }
+        }
+    }
+};
