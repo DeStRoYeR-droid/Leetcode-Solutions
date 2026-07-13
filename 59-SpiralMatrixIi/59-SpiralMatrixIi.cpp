1fdc82d342ec6a7d95013775aa40dda3f0ed4483
Immanuel Vivek <63895010+DeStRoYeR-droid@users.noreply.github.com>
2026-07-13 22:28:54 +0530

    Create 59-SpiralMatrixIi

---
+++ 59-SpiralMatrixIi/59-SpiralMatrixIi.cpp
@@ -0,0 +1,34 @@
+// Last updated: 13/07/2026, 22:28:52
+class Solution {
+public:
+    vector<vector<int>> generateMatrix(const int& n) {
+        vector<vector<int>> result(n, vector<int>(n));
+        int top = 0, bottom = n - 1;
+        int left = 0, right = n - 1;
+        int num = 1;
+
+        while (top <= bottom && left <= right){
+            for (int i = left; i <= right; i++) {
+                result[top][i] = num++;
+            }
+            top++;
+            for (int i = top; i <= bottom; i++) {
+                result[i][right] = num++;
+            }
+            right--;
+            if (top <= bottom) {
+                for (int i = right; i >= left; i--) {
+                    result[bottom][i] = num++;
+                }
+                bottom--;
+            }
+            if (left <= right) {
+                for (int i = bottom; i >= top; i--) {
+                    result[i][left] = num++;
+                }
+                left++;
+            }
+        }
+        return result;
+    }
+};
