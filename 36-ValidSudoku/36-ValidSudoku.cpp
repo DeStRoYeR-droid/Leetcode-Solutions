421aa8b18022044758e77613e4e2d6537216e126
Immanuel Vivek <63895010+DeStRoYeR-droid@users.noreply.github.com>
2026-07-13 22:29:42 +0530

    Create 36-ValidSudoku

---
+++ 36-ValidSudoku/36-ValidSudoku.cpp
@@ -0,0 +1,31 @@
+// Last updated: 13/07/2026, 22:29:40
+class Solution {
+public:
+    bool isValidSudoku(vector<vector<char>>& board) {
+        for (int i = 0; i < 9; i++){
+            for (int j = 0; j < 9; j++){
+                if (board[i][j] != '.'){
+                    if (!(validPosition(board , board[i][j] , i, j))){
+                        return false;
+                    }
+                }
+            }
+        }
+        return true;
+    }
+    bool validPosition(vector<vector<char>>& board , char num, int row, int col){
+        for (int i = 0; i < 9; i++) if (i != col && board[row][i] == num) return false;
+        for (int i = 0; i < 9; i++) if (i != row && board[i][col] == num) return false;
+        
+        int base_r = (row / 3) * 3;
+        int base_c = (col / 3) * 3;
+        
+        for (int i = 0; i < 3; i++){
+            for (int j = 0; j < 3; j++) 
+                if (base_r + i != row && 
+                    base_c + j != col &&
+                    board[base_r + i][base_c + j] == num) return false;
+        }
+        return true;
+    }
+};
