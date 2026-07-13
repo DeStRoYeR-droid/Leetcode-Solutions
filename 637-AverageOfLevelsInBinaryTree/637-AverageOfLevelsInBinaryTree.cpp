dd35e2e231d903ac0d090d6252a26222fb57854c
Immanuel Vivek <63895010+DeStRoYeR-droid@users.noreply.github.com>
2026-07-13 22:17:40 +0530

    Create 637-AverageOfLevelsInBinaryTree

---
+++ 637-AverageOfLevelsInBinaryTree/637-AverageOfLevelsInBinaryTree.cpp
@@ -0,0 +1,37 @@
+// Last updated: 13/07/2026, 22:17:38
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<double> averageOfLevels(TreeNode* root) {
+        vector<double> result;
+        if (root == nullptr) return result;
+        queue<TreeNode*> q;
+        q.push(root);
+        
+        while (!q.empty()){
+            int size = q.size();
+            int copy = size;
+            vector<int> row;
+            row.reserve(size);
+            double cur_sum = 0.0;
+            while (size--){
+                TreeNode* cur = q.front(); q.pop();
+                cur_sum += cur->val;
+                if (cur->left) q.push(cur->left);
+                if (cur->right) q.push(cur->right);
+            }
+            result.push_back(cur_sum / copy);
+        }
+        return result;
+    }
+};
