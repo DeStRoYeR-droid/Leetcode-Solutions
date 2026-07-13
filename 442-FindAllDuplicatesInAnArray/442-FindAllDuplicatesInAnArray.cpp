03c16e5817d8f1c1c54b2138505029a4ee4bb926
Immanuel Vivek <63895010+DeStRoYeR-droid@users.noreply.github.com>
2026-07-13 22:19:40 +0530

    Create 442-FindAllDuplicatesInAnArray

---
+++ 442-FindAllDuplicatesInAnArray/442-FindAllDuplicatesInAnArray.cpp
@@ -0,0 +1,14 @@
+// Last updated: 13/07/2026, 22:19:38
+class Solution {
+public:
+    vector<int> findDuplicates(vector<int>& nums) {
+        unordered_set<int> seen;
+        vector<int> result;
+        for (int& num : nums){
+            if (seen.find(num) == seen.end()) seen.insert(num);
+            else result.push_back(num);
+        }
+
+        return result;
+    }
+};
