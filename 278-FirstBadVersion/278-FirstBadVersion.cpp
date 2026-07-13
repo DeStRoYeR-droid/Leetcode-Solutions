38cd0147da93edb16b21732d0f487aaed21d0fb9
Immanuel Vivek <63895010+DeStRoYeR-droid@users.noreply.github.com>
2026-07-13 22:21:50 +0530

    Create 278-FirstBadVersion

---
+++ 278-FirstBadVersion/278-FirstBadVersion.cpp
@@ -0,0 +1,17 @@
+// Last updated: 13/07/2026, 22:21:48
+// The API isBadVersion is defined for you.
+// bool isBadVersion(int version);
+
+class Solution {
+public:
+    int firstBadVersion(int n) {
+        int left = 1, right = n;
+        while (left <= right){
+            int mid = left + (right - left) / 2;
+            if (isBadVersion(mid) && !isBadVersion(mid - 1)) return mid;
+            else if (isBadVersion(mid)) right = mid;
+            else left = mid + 1;
+        }
+        return left;
+    }
+};
