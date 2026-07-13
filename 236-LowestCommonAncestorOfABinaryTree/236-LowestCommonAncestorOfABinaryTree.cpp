5533bbe97b75cde7bff33542e4074529e9c97801
Immanuel Vivek <63895010+DeStRoYeR-droid@users.noreply.github.com>
2026-07-13 22:22:30 +0530

    Create 236-LowestCommonAncestorOfABinaryTree

---
+++ 236-LowestCommonAncestorOfABinaryTree/236-LowestCommonAncestorOfABinaryTree.cpp
@@ -0,0 +1,22 @@
+// Last updated: 13/07/2026, 22:22:28
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, const TreeNode* p, const TreeNode* q) {
+        if (root == nullptr || root == p || root == q) return root;
+
+        TreeNode* left = lowestCommonAncestor(root->left, p, q);
+        TreeNode* right = lowestCommonAncestor(root->right, p, q);
+
+        if (left && right) return root;
+        return (!left) ? right : left;
+    }
+};
