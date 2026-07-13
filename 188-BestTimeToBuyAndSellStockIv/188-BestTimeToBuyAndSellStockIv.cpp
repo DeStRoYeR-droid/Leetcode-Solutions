33fd3c57313cc4b5aae0d383e41cab461173d54b
Immanuel Vivek <63895010+DeStRoYeR-droid@users.noreply.github.com>
2026-07-13 22:24:18 +0530

    Create 188-BestTimeToBuyAndSellStockIv

---
+++ 188-BestTimeToBuyAndSellStockIv/188-BestTimeToBuyAndSellStockIv.cpp
@@ -0,0 +1,19 @@
+// Last updated: 13/07/2026, 22:24:16
+class Solution {
+public:
+    int maxProfit(int k, vector<int>& prices) {
+        if (k == 0) return 0;
+
+        vector<vector<int>> dp(k + 1, vector<int>(2));
+        for (int i = 0; i <= k; ++i) dp[i][0] = INT_MAX;
+
+        for (const int& price : prices){
+            for (int i = 1; i <= k; ++i){
+                dp[i][0] = min(dp[i][0], price - dp[i - 1][1]);
+                dp[i][1] = max(dp[i][1], price - dp[i][0]);
+            }
+        }
+
+        return dp[k][1];
+    }
+};
