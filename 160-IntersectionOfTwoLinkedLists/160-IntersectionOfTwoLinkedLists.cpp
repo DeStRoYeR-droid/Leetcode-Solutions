e8852c9c94b6e1409c47fd6ca2e5563bfb82e1d8
Immanuel Vivek <63895010+DeStRoYeR-droid@users.noreply.github.com>
2026-07-13 22:25:32 +0530

    Create 160-IntersectionOfTwoLinkedLists

---
+++ 160-IntersectionOfTwoLinkedLists/160-IntersectionOfTwoLinkedLists.cpp
@@ -0,0 +1,22 @@
+// Last updated: 13/07/2026, 22:25:30
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+        ListNode* lista = headA;
+        ListNode* listb = headB;
+
+        while (lista != listb){
+            lista = (lista != nullptr) ? lista->next : headB;
+            listb = (listb != nullptr) ? listb->next : headA;
+        }
+        return lista;
+    }
+};
