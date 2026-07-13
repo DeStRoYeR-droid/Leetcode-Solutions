7ebe431bb8fd25baff14573475469ef7604b7c7c
Immanuel Vivek <63895010+DeStRoYeR-droid@users.noreply.github.com>
2026-07-13 22:21:07 +0530

    Create 331-VerifyPreorderSerializationOfABinaryTree

---
+++ 331-VerifyPreorderSerializationOfABinaryTree/331-VerifyPreorderSerializationOfABinaryTree.cpp
@@ -0,0 +1,16 @@
+// Last updated: 13/07/2026, 22:21:06
+class Solution {
+public:
+    bool isValidSerialization(string preorder) {
+        stringstream ss(preorder);
+        string curr;
+        int nodes = 1;
+        
+        while (getline(ss, curr, ',')){
+            --nodes;
+            if (nodes < 0) return false;
+            if (curr != "#") nodes += 2;
+        }
+        return nodes == 0;
+    }
+};
