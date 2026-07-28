343ec255843fe6d7dd93880fc818b144dfcf14d3
Immanuel Vivek <63895010+DeStRoYeR-droid@users.noreply.github.com>
2026-07-28 23:01:38 +0530

    Create 3812-SmallestPalindromicRearrangementI

---
+++ 3812-SmallestPalindromicRearrangementI/3812-SmallestPalindromicRearrangementI.cpp
@@ -0,0 +1,20 @@
+// Last updated: 28/07/2026, 23:01:37
+class Solution {
+public:
+    string smallestPalindrome(string s) {
+        int n = s.size(), j = 0;
+        int freq[26]{};
+
+        for (int i = 0; i < n >> 1; i++)            
+            freq[(s[i] & 31) - 1]++;
+
+        for (int i = 0; i < 26; i++) {
+            while (freq[i]--) {                
+                s[j] = 'a' + i;          
+                s[n - 1 - j++] = 'a' + i;
+            }
+        }
+
+        return s;
+    }
+};
