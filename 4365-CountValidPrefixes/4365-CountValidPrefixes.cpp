97a966e094614e6ea4c48c6aa56f03b51c739a3e
Immanuel Vivek <63895010+DeStRoYeR-droid@users.noreply.github.com>
2026-08-02 14:01:22 +0530

    Create 4365-CountValidPrefixes

---
+++ 4365-CountValidPrefixes/4365-CountValidPrefixes.cpp
@@ -0,0 +1,15 @@
+// Last updated: 02/08/2026, 14:01:21
+class Solution {
+public:
+    int countValidPrefixes(string s) {
+        int c_1 = 0, c_0 = 0;
+        int result = 0;
+        for (const char& c : s){
+            c_1 += (c == '1');
+            c_0 += (c == '0');
+
+            result += (bool)(abs(c_0 - c_1) <= 1);
+        }
+        return result;
+    }
+};
