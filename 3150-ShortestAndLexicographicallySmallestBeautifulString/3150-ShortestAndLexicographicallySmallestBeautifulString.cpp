5f2bf890492b9d358421f1e67bdebf0ac7b50a92
Immanuel Vivek Tirkey <63895010+DeStRoYeR-droid@users.noreply.github.com>
2026-08-27 11:20:46 +0530

    Create 3150-ShortestAndLexicographicallySmallestBeautifulString

---
+++ 3150-ShortestAndLexicographicallySmallestBeautifulString/3150-ShortestAndLexicographicallySmallestBeautifulString.cpp
@@ -0,0 +1,31 @@
+// Last updated: 27/08/2026, 11:20:46
+class Solution {
+public:
+    string shortestBeautifulSubstring(string s, int k) {
+        vector<int> pos;
+        for (int i = 0; i < s.size(); i++) {
+            if (s[i] == '1') {
+                pos.push_back(i);
+            }
+        }
+
+        if (pos.size() < k)
+            return "";
+
+        string ans = "";
+
+        for (int i = 0; i + k - 1 < pos.size(); i++) {
+            int start = pos[i];
+            int end = pos[i + k - 1];
+            string cur = s.substr(start, end - start + 1);
+            if (ans == "" ||
+                cur.size() < ans.size() ||
+                (cur.size() == ans.size() && cur < ans)) {
+
+                ans = cur;
+            }
+        }
+
+        return ans;
+    }
+};
