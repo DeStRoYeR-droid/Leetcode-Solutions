813d89292cd0f043d11fc8ee3130250d04365053
Immanuel Vivek Tirkey <63895010+DeStRoYeR-droid@users.noreply.github.com>
2026-09-05 16:37:20 +0530

    Create 4039-SumOfDecodedNumbers

---
+++ 4039-SumOfDecodedNumbers/4039-SumOfDecodedNumbers.cpp
@@ -0,0 +1,34 @@
+// Last updated: 05/09/2026, 16:37:19
+static const int MOD = 1e9 + 7;
+
+long long binpow(long long a, long long b, long long m = MOD) {
+    long long res = 1;
+    while (b > 0) {
+        if (b & 1)
+            res = res * a % m;
+        a = a * a % m;
+        b >>= 1;
+    }
+    return res;
+}
+
+class Solution {
+public:
+    int sumDecoded(vector<long long>& nums) {
+        int result = 0;
+        for (const long long& val : nums){
+            long long width = val % 10;
+            string s = to_string(val);
+
+            long long base = 0, exponent = 0;
+            for (int i = 0; i < width; ++i){
+                base = base * 10 + (s[i] - '0');
+            }
+            for (int i = width; i < s.size() - 1; ++i){
+                exponent = exponent * 10 + (s[i] - '0');
+            }
+            result = (result + binpow(base, exponent)) % MOD;
+        }
+        return result;
+    }
+};
