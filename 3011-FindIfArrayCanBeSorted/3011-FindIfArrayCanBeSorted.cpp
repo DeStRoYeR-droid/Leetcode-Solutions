94ac228a5cb45e2a531b203cf793d34633635847
Immanuel Vivek Tirkey <63895010+DeStRoYeR-droid@users.noreply.github.com>
2026-09-07 22:38:39 +0530

    Create 3011-FindIfArrayCanBeSorted

---
+++ 3011-FindIfArrayCanBeSorted/3011-FindIfArrayCanBeSorted.cpp
@@ -0,0 +1,25 @@
+// Last updated: 07/09/2026, 22:38:38
+class Solution {
+public:
+    int get_set_bits(int& n){
+        int result = 0;
+        for (int i = 0; i < 32; i++) result += ((n & 1 << i) != 0);
+        return result;
+    }
+    bool canSortArray(vector<int>& nums) {
+        const int n = nums.size();
+        vector<int> set_bits(n, 0);
+        
+        for (int i = 0; i < n; i++) { set_bits[i] = get_set_bits(nums[i]);}
+
+        for (int i = 0; i < n - 1; i++){
+            for (int j = i+1; j < n; j++){
+                if (nums[i] > nums[j]) 
+                    if (set_bits[i] != set_bits[j]){
+                        return false;
+                    }
+            }
+        }
+        return true;
+    }
+};
