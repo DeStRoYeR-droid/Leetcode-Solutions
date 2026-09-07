fe3ad0bf810ad97a65db4eb367ec7b656622f3b5
Immanuel Vivek Tirkey <63895010+DeStRoYeR-droid@users.noreply.github.com>
2026-09-07 22:51:02 +0530

    Create 2070-MostBeautifulItemForEachQuery

---
+++ 2070-MostBeautifulItemForEachQuery/2070-MostBeautifulItemForEachQuery.py
@@ -0,0 +1,24 @@
+# Last updated: 07/09/2026, 22:51:02
+class Solution:
+    def maximumBeauty(self, items: List[List[int]], queries: List[int]) -> List[int]:
+        max_I = float('inf')
+        res = [[0, 0, max_I]]
+
+        items.sort(key = lambda x : x[0])
+
+        for price, beauty in items:
+            last_bracket = res[-1]
+            if (beauty > last_bracket[1]):
+                res[-1][2] = price
+                res.append([price, beauty, max_I])
+
+        print (res)
+        ans = []
+        for x in queries:
+            for i in range(len(res) - 1, -1, -1):
+                if (res[i][0] <= x):
+                    ans.append(res[i][1])
+                    break
+        print (ans)
+
+        return ans
