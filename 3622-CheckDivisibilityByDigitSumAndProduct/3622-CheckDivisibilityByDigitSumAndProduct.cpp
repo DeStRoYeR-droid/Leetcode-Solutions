58fda2a2dcdd36131247167785beb2f6ea28e1a2
Immanuel Vivek Tirkey <63895010+DeStRoYeR-droid@users.noreply.github.com>
2026-09-07 22:29:10 +0530

    Create 3622-CheckDivisibilityByDigitSumAndProduct

---
+++ 3622-CheckDivisibilityByDigitSumAndProduct/3622-CheckDivisibilityByDigitSumAndProduct.cpp
@@ -0,0 +1,14 @@
+// Last updated: 07/09/2026, 22:29:10
+class Solution {
+public:
+    static auto checkDivisibility(int n) -> bool {
+        int r1 = 0, r2 = 1;
+        int c = n;
+        while (c){
+            r1 += c % 10;
+            r2 *= c % 10;
+            c /= 10;
+        }
+        return n % (r1 + r2) == 0;
+    }
+};
