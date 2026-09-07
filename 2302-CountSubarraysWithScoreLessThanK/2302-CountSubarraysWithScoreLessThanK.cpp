9f0a0747717006f03a972f62d3e9916d0ea8bed0
Immanuel Vivek Tirkey <63895010+DeStRoYeR-droid@users.noreply.github.com>
2026-09-07 22:47:14 +0530

    Create 2302-CountSubarraysWithScoreLessThanK

---
+++ 2302-CountSubarraysWithScoreLessThanK/2302-CountSubarraysWithScoreLessThanK.cpp
@@ -0,0 +1,21 @@
+// Last updated: 07/09/2026, 22:47:14
+class Solution {
+public:
+    long long countSubarrays(vector<int>& nums, long long k) {
+        const int n = nums.size();
+        int left = 0, right = 0;
+        long long result = 0, curSum = 0;
+        
+        for (right; right < n; ++right){
+            curSum += nums[right];
+            while (curSum * (right - left + 1) >= k){
+                curSum -= nums[left];
+                ++left;
+            }
+
+            result += (right - left + 1);
+        }
+        
+        return result;
+    }
+};
