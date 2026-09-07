7a7331634d89f8ec54f643b8669fd85edb34881d
Immanuel Vivek Tirkey <63895010+DeStRoYeR-droid@users.noreply.github.com>
2026-09-07 22:22:35 +0530

    Create 3861-MinimumCapacityBox

---
+++ 3861-MinimumCapacityBox/3861-MinimumCapacityBox.cpp
@@ -0,0 +1,18 @@
+// Last updated: 07/09/2026, 22:22:35
+typedef pair<int, int> pii;
+class Solution {
+public:
+    int minimumIndex(vector<int>& capacity, int itemSize) {
+        vector<pii> v;
+        for (int i = 0; i < capacity.size(); ++i){
+            v.push_back({capacity[i], i});
+        }
+        sort(v.begin(), v.end());
+        for (int i = 0; i < capacity.size(); ++i){
+            if (itemSize <= v[i].first){
+                return v[i].second;
+            }
+        }
+        return -1;
+    }
+};
