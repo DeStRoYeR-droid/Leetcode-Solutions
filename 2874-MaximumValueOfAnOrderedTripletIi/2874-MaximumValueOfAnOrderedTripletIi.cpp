8edd67664dab2410f6883df1b664fa678c6b6314
Immanuel Vivek Tirkey <63895010+DeStRoYeR-droid@users.noreply.github.com>
2026-09-07 22:40:22 +0530

    Create 2874-MaximumValueOfAnOrderedTripletIi

---
+++ 2874-MaximumValueOfAnOrderedTripletIi/2874-MaximumValueOfAnOrderedTripletIi.cpp
@@ -0,0 +1,13 @@
+// Last updated: 07/09/2026, 22:40:22
+class Solution {
+public:
+    long long maximumTripletValue(vector<int>& nums) {
+        long long result = 0, maxVal = 0, maxDiff = 0;
+        for (long long num : nums){
+            result = max(result, maxDiff * num);
+            maxDiff = max(maxDiff, maxVal - num);
+            maxVal = max(maxVal, num);
+        }
+        return result;
+    }
+};
