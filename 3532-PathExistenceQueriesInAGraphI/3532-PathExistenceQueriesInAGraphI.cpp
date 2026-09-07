52642582a99b55ac655cd3acdd697e5949eef9fb
Immanuel Vivek Tirkey <63895010+DeStRoYeR-droid@users.noreply.github.com>
2026-09-07 22:30:25 +0530

    Create 3532-PathExistenceQueriesInAGraphI

---
+++ 3532-PathExistenceQueriesInAGraphI/3532-PathExistenceQueriesInAGraphI.cpp
@@ -0,0 +1,21 @@
+// Last updated: 07/09/2026, 22:30:25
+class Solution {
+public:
+    vector<bool> pathExistenceQueries(int n, vector<int>& nums, int maxDiff, vector<vector<int>>& queries) {
+        vector<int> group(n, 0);
+        int curr = 0;
+
+        for (int i = 1; i < n; ++i){
+            if (nums[i] - nums[i - 1] > maxDiff) ++curr;
+            group[i] = curr;
+        }
+
+        vector<bool> result;
+        result.reserve(n);
+
+        for (const auto& q: queries){
+            result.push_back(group[q[0]] == group[q[1]]);
+        }
+        return result;
+    }
+};
