868a3370bc012cb581770d78968f1be05410d3a6
Immanuel Vivek Tirkey <63895010+DeStRoYeR-droid@users.noreply.github.com>
2026-09-07 22:41:58 +0530

    Create 2667-CreateHelloWorldFunction

---
+++ 2667-CreateHelloWorldFunction/2667-CreateHelloWorldFunction.js
@@ -0,0 +1,15 @@
+// Last updated: 07/09/2026, 22:41:58
+/**
+ * @return {Function}
+ */
+var createHelloWorld = function() {
+    
+    return function(...args) {
+        return "Hello World";
+    }
+};
+
+/**
+ * const f = createHelloWorld();
+ * f(); // "Hello World"
+ */
