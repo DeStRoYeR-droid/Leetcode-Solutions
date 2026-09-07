8093ef226e6477f7bf6379c1aaae775017085287
Immanuel Vivek Tirkey <63895010+DeStRoYeR-droid@users.noreply.github.com>
2026-09-07 22:39:45 +0530

    Create 2946-MatrixSimilarityAfterCyclicShifts

---
+++ 2946-MatrixSimilarityAfterCyclicShifts/2946-MatrixSimilarityAfterCyclicShifts.cpp
@@ -0,0 +1,20 @@
+// Last updated: 07/09/2026, 22:39:44
+class Solution {
+public:
+    bool areSimilar(vector<vector<int>>& mat, int k) {
+        int n = mat.size(), m = mat[0].size();
+        k = k % m;
+
+        for (int i = 0; i < n; ++i){
+            for (int j = 0; j < m; ++j){
+                if (i % 2){
+                    if (mat[i][(j + k) % m] != mat[i][j]) return false;
+                }
+                else{
+                    if (mat[i][(j - k + m) % m] != mat[i][j]) return false;
+                }
+            }
+        }
+        return true;;
+    }
+};
