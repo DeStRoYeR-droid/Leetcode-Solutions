bd07826aa9dab6958d458a2d6832bcfbc242f1ef
Immanuel Vivek Tirkey <63895010+DeStRoYeR-droid@users.noreply.github.com>
2026-09-07 22:37:26 +0530

    Create 3110-ScoreOfAString

---
+++ 3110-ScoreOfAString/3110-ScoreOfAString.cpp
@@ -0,0 +1,9 @@
+// Last updated: 07/09/2026, 22:37:26
+class Solution {
+public:
+    int scoreOfString(string s) {
+        int result = 0;
+        for (int i = 0; i < s.size() - 1; ++i) result += abs(int(s[i]) - s[i + 1]);
+        return result;
+    }
+};
