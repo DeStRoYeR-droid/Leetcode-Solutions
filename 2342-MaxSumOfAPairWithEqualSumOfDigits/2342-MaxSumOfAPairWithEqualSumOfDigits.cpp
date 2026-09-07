ed376b9004f83ed338a1a686d2cf940cc2aee4f5
Immanuel Vivek Tirkey <63895010+DeStRoYeR-droid@users.noreply.github.com>
2026-09-07 22:45:55 +0530

    Create 2342-MaxSumOfAPairWithEqualSumOfDigits

---
+++ 2342-MaxSumOfAPairWithEqualSumOfDigits/2342-MaxSumOfAPairWithEqualSumOfDigits.cpp
@@ -0,0 +1,26 @@
+// Last updated: 07/09/2026, 22:45:54
+class Solution {
+    vector<int> digits;
+public:
+    Solution() : digits(82, 0){}
+    int getDigitSum(int num){
+        int result = 0;
+        do{
+            result += num % 10;
+            num = num / 10;
+        } while (num > 0);
+        return result;
+    }
+
+    int maximumSum(vector<int>& nums) {
+        int result = -1;
+        for (const int& num : nums){
+            int digitSum = getDigitSum(num);
+            if (digits[digitSum] != 0){
+                result = max(result, digits[digitSum] + num);
+            }
+            digits[digitSum] = max(digits[digitSum], num);
+        }
+        return result;
+    }
+};
