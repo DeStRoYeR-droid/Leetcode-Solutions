c0e9290bef00c1cbf07bd2ee2ed4a9e928170170
Immanuel Vivek Tirkey <63895010+DeStRoYeR-droid@users.noreply.github.com>
2026-09-07 22:51:14 +0530

    Create 2044-CountNumberOfMaximumBitwiseOrSubsets

---
+++ 2044-CountNumberOfMaximumBitwiseOrSubsets/2044-CountNumberOfMaximumBitwiseOrSubsets.cpp
@@ -0,0 +1,19 @@
+// Last updated: 07/09/2026, 22:51:14
+class Solution {
+public:
+    int countMaxOrSubsets(const vector<int>& nums) {
+        int maxVal = 0;
+        for (const int& num : nums) maxVal |= num;
+        return backtrack(nums, maxVal, 0, 0);
+    }
+
+    int backtrack(const vector<int>& nums, int maxVal, int index, int curVal){
+        if (index == nums.size()){
+            return (curVal == maxVal) ? 1 : 0;
+        }
+        if (curVal == maxVal){
+            return 1 << (nums.size() - index);
+        }
+        return backtrack(nums, maxVal, index + 1, curVal | nums[index]) + backtrack(nums, maxVal, index + 1, curVal);
+    }
+};
