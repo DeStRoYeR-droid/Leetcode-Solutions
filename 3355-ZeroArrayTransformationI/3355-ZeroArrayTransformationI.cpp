a4321ba511dc1860c419698daa24ebcd243cbfe3
Immanuel Vivek Tirkey <63895010+DeStRoYeR-droid@users.noreply.github.com>
2026-09-07 22:33:51 +0530

    Create 3355-ZeroArrayTransformationI

---
+++ 3355-ZeroArrayTransformationI/3355-ZeroArrayTransformationI.cpp
@@ -0,0 +1,19 @@
+// Last updated: 07/09/2026, 22:33:50
+class Solution {
+public:
+    bool isZeroArray(vector<int>& nums, vector<vector<int>>& queries) {
+        const int n = nums.size();
+        vector<int> diff(n+1);
+        for (auto& q : queries){
+            ++diff[q[0]];
+            if (q[1] + 1 < n) --diff[q[1] + 1];
+        }
+
+        int total = 0;
+        for (int i = 0; i < n; ++i){
+            total += diff[i];
+            if (nums[i] > total) return false;
+        }
+        return true;
+    }
+};
