e56d83d1d0cf8b63d0214ca35093a44bdfbb0149
Immanuel Vivek Tirkey <63895010+DeStRoYeR-droid@users.noreply.github.com>
2026-09-07 22:30:11 +0530

    Create 3546-EqualSumGridPartitionI

---
+++ 3546-EqualSumGridPartitionI/3546-EqualSumGridPartitionI.cpp
@@ -0,0 +1,35 @@
+// Last updated: 07/09/2026, 22:30:11
+class Solution {
+public:
+    bool canPartitionGrid(vector<vector<int>>& grid) {
+        int n = grid.size(), m = grid[0].size();
+
+        vector<long long> r(n, 0LL), c(m, 0LL);
+
+        for (int i = 0; i < n; ++i){
+            r[i] += accumulate(grid[i].begin(), grid[i].end(), 0LL);
+            for (int j = 0; j < m; ++j){
+                c[j] += grid[i][j];
+            }
+        }
+
+        long long rt = accumulate(r.begin(), r.end(), 0LL), ct = accumulate(c.begin(), c.end(), 0LL);
+        long long rn = 0, cn = 0;
+
+        for (int i = 0; i < n; ++i){
+            rt -= r[i];
+            rn += r[i];
+
+            if (rt == rn) return true;
+        }
+
+        for (int i = 0; i < m; ++i){
+            ct -= c[i];
+            cn += c[i];
+
+            if (ct == cn) return true;
+        }
+
+        return false;
+    }
+};
