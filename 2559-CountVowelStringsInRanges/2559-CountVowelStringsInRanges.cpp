3d6d3cfaff9b98c4fc0de48f48e511c99193737f
Immanuel Vivek Tirkey <63895010+DeStRoYeR-droid@users.noreply.github.com>
2026-09-07 22:42:59 +0530

    Create 2559-CountVowelStringsInRanges

---
+++ 2559-CountVowelStringsInRanges/2559-CountVowelStringsInRanges.cpp
@@ -0,0 +1,22 @@
+// Last updated: 07/09/2026, 22:42:58
+class Solution {
+public:
+    vector<int> vowelStrings(vector<string>& words, vector<vector<int>>& queries) {
+        vector<int> prefix(words.size() + 1, 0);
+        vector<int> result(queries.size());
+
+        int checker = 1065233;
+        int i;
+        for (i = 0; i < words.size(); i++){
+            prefix[i+1] = prefix[i] + (
+                        ((1 << (words[i].front() - 'a')) & checker) && 
+                        ((1 << (words[i].back() - 'a')) & checker)
+            );
+        }
+        
+        i = 0;
+        for (auto& q : queries) result[i++] = prefix[q[1] + 1] - prefix[q[0]];
+        
+        return result;
+    }
+};
