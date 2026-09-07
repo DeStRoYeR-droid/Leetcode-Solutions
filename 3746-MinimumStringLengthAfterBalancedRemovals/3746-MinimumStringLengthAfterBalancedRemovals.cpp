2a804ae472e33d84de0958e14f17fd81595ffe7f
Immanuel Vivek Tirkey <63895010+DeStRoYeR-droid@users.noreply.github.com>
2026-09-07 22:25:01 +0530

    Create 3746-MinimumStringLengthAfterBalancedRemovals

---
+++ 3746-MinimumStringLengthAfterBalancedRemovals/3746-MinimumStringLengthAfterBalancedRemovals.cpp
@@ -0,0 +1,25 @@
+// Last updated: 07/09/2026, 22:25:00
+class Solution {
+public:
+    int minLengthAfterRemovals(string s) {
+        const int n = s.size();
+        int remove = 0;
+        int cA = 0, cB = 0;
+        for (const char& ch : s){
+            if (ch == 'a') ++cA;
+            else ++cB;
+            if (cA == cB){
+                remove += cA * 2;
+                cA = 0;
+                cB = 0;
+            }
+            else if (cA != 0 && cB != 0){
+                int mini = min(cA, cB);
+                remove += 2 * mini;
+                cA -= mini;
+                cB -= mini;
+            }
+        }
+        return n - remove;
+    }
+};
