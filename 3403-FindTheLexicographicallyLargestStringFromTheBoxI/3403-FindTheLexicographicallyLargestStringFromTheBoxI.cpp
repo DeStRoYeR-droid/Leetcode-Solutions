fbaea9fc2a2f06e82188a63cabfb4ea419244630
Immanuel Vivek Tirkey <63895010+DeStRoYeR-droid@users.noreply.github.com>
2026-09-07 22:33:09 +0530

    Create 3403-FindTheLexicographicallyLargestStringFromTheBoxI

---
+++ 3403-FindTheLexicographicallyLargestStringFromTheBoxI/3403-FindTheLexicographicallyLargestStringFromTheBoxI.cpp
@@ -0,0 +1,12 @@
+// Last updated: 07/09/2026, 22:33:09
+class Solution {
+public:
+    string answerString(string word, int numFriends) {
+        if (numFriends == 1) return word;
+        string result = "";
+        for (int i = 0; i < word.size(); ++i){
+            result = max(result, word.substr(i, word.length() - numFriends + 1));
+        }
+        return result;
+    }
+};
