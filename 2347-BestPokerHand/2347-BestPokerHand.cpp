0d413e0fa2291b3dfbffef026e605422d35f651e
Immanuel Vivek Tirkey <63895010+DeStRoYeR-droid@users.noreply.github.com>
2026-09-07 22:46:25 +0530

    Create 2347-BestPokerHand

---
+++ 2347-BestPokerHand/2347-BestPokerHand.cpp
@@ -0,0 +1,26 @@
+// Last updated: 07/09/2026, 22:46:24
+class Solution {
+public:
+    string bestHand(vector<int>& ranks, vector<char>& suits) {
+        bool isFlush = true, pair = false;
+        for (int i = 1; i < 5; ++i) {
+            if (suits[i] != suits[0]){
+                isFlush = false;
+                break;
+            }
+        }
+
+        if (isFlush) return "Flush";
+
+        vector<int> mapping(13, 0);
+        for (const int& val : ranks) mapping[val - 1]++;
+
+        for (int i = 0; i < 13; ++i){
+            if (mapping[i] >= 3) return "Three of a Kind";
+            if (mapping[i] == 2) pair = true;
+        }
+        if (pair) return "Pair";
+
+        return "High Card";
+    }
+};
