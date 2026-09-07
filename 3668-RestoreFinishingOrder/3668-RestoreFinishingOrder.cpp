5274beb484ceebf2b4ea49b0bc1d9f225cc7b950
Immanuel Vivek Tirkey <63895010+DeStRoYeR-droid@users.noreply.github.com>
2026-09-07 22:27:00 +0530

    Create 3668-RestoreFinishingOrder

---
+++ 3668-RestoreFinishingOrder/3668-RestoreFinishingOrder.cpp
@@ -0,0 +1,24 @@
+// Last updated: 07/09/2026, 22:27:00
+class Solution {
+public:
+    vector<int> recoverOrder(vector<int>& order, vector<int>& friends) {
+        vector<int> result;
+        for (const int& val : order){
+            int index = binarySearch(friends, val);
+            if (index != -1){
+                result.push_back(friends[index]);
+            }
+        }
+        return result;
+    }
+    int binarySearch(const vector<int>& arr, const int& val){
+        int left = 0, right = arr.size() - 1;
+        while (left <= right){
+            int mid = left + (right - left) / 2;
+            if (arr[mid] == val) return mid;
+            else if (arr[mid] > val) right = mid - 1;
+            else left = mid + 1;
+        }
+        return -1;
+    }
+};
