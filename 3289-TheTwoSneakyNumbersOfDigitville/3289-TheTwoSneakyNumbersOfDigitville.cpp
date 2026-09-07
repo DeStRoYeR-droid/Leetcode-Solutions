0cd8b2b4f75781c3db637ef542f80ab00d1e2188
Immanuel Vivek Tirkey <63895010+DeStRoYeR-droid@users.noreply.github.com>
2026-09-07 22:35:20 +0530

    Create 3289-TheTwoSneakyNumbersOfDigitville

---
+++ 3289-TheTwoSneakyNumbersOfDigitville/3289-TheTwoSneakyNumbersOfDigitville.cpp
@@ -0,0 +1,25 @@
+// Last updated: 07/09/2026, 22:35:19
+class Solution {
+public:
+    vector<int> getSneakyNumbers(const vector<int>& nums) {
+        const int n = nums.size() - 2;
+        int value = 0;
+        for (int i = 0; i < n; ++i) value ^= i;
+        for (const auto& val : nums) value ^= val;
+        
+        value = value & -value;
+
+        int xor1 = 0, xor2 = 0;
+        for (const auto& val : nums){
+            if (value & val) xor1 ^= val;
+            else xor2 ^= val;
+        }
+
+        for (int i = 0; i < n; ++i){
+            if (i & value) xor1 ^= i;
+            else xor2 ^= i;
+        }
+
+        return {xor1, xor2};
+    }
+};
