982b73e6d3c6536eeb72c93f5df068b25bca6a88
Immanuel Vivek Tirkey <63895010+DeStRoYeR-droid@users.noreply.github.com>
2026-09-14 09:30:16 +0530

    Create 1061-LexicographicallySmallestEquivalentString

---
+++ 1061-LexicographicallySmallestEquivalentString/1061-LexicographicallySmallestEquivalentString.cpp
@@ -0,0 +1,33 @@
+// Last updated: 14/09/2026, 09:30:15
+class Solution {
+public:
+    string smallestEquivalentString(string s1, string s2, string baseStr) {
+        const int n = s1.size();
+        unordered_map<char, vector<char>> adj;
+
+        for (int i = 0; i < n; ++i){
+            char u = s1[i], v = s2[i];
+            adj[u].push_back(v);
+            adj[v].push_back(u);
+        }
+
+        string result = "";
+        for (const char& ch : baseStr){
+            vector<bool> vis(26, 0);
+            char minChar = dfs(adj, ch, vis);
+            result.push_back(minChar);
+        }
+        return result;
+    }
+
+    char dfs(unordered_map<char, vector<char>>& adj, char cur, vector<bool>& vis){
+        vis[cur - 'a'] = true;
+        char minChar = cur;
+        for (char& neighbor : adj[cur]){
+            if (!vis[neighbor - 'a']){
+                minChar = min(minChar, dfs(adj, neighbor, vis));
+            }
+        }
+        return minChar;
+    }
+};
