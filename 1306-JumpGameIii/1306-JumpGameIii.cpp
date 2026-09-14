2914b63e12ececf24c3947d4539d06b24e37eaf3
Immanuel Vivek Tirkey <63895010+DeStRoYeR-droid@users.noreply.github.com>
2026-09-14 09:25:23 +0530

    Create 1306-JumpGameIii

---
+++ 1306-JumpGameIii/1306-JumpGameIii.cpp
@@ -0,0 +1,27 @@
+// Last updated: 14/09/2026, 09:25:23
+class Solution {
+public:
+    bool canReach(vector<int>& arr, int start) {
+        int n=arr.size();
+        queue<int>q; 
+        vector<int>vis(n,0);
+        q.push(start);
+        vis[start]=1;
+        while(!q.empty()){
+            auto tp=q.front();
+            q.pop(); 
+            int l=tp-arr[tp];
+            int r=tp+arr[tp];
+            if(!arr[tp])return 1;
+            if(l>=0&&!vis[l]){
+                vis[l]=1;
+                q.push(l); 
+            }
+            if(r<n&&!vis[r]){
+                vis[r]=1;
+                q.push(r); 
+            }
+        } 
+        return 0;
+    }
+};
