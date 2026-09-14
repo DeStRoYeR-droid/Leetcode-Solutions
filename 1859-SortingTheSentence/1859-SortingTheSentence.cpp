a9b43ed3e8d965b56d415db20242af17dd0be563
Immanuel Vivek Tirkey <63895010+DeStRoYeR-droid@users.noreply.github.com>
2026-09-14 09:16:25 +0530

    Create 1859-SortingTheSentence

---
+++ 1859-SortingTheSentence/1859-SortingTheSentence.cpp
@@ -0,0 +1,17 @@
+// Last updated: 14/09/2026, 09:16:25
+class Solution {
+public:
+    string sortSentence(string s) {
+        stringstream ss(s);
+        string word, result = "";
+        vector<string> res(9);
+        while (ss >> word){
+            int index = (word[word.size() - 1] - '1');
+            res[index] = word.substr(0, word.size() - 1);
+        }
+        for (const string& word : res){
+            if (word != "") result += word + " ";
+        }
+        return result.substr(0, result.size() -1);
+    }
+};
