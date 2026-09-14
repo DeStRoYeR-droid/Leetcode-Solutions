cd70f50164eb8e9eed2e83c31e4211aaff7ba3c4
Immanuel Vivek Tirkey <63895010+DeStRoYeR-droid@users.noreply.github.com>
2026-09-14 09:23:23 +0530

    Create 1399-CountLargestGroup

---
+++ 1399-CountLargestGroup/1399-CountLargestGroup.cpp
@@ -0,0 +1,24 @@
+// Last updated: 14/09/2026, 09:23:23
+class Solution {
+private:
+    int digitSum(int n){
+        int result = 0;
+        while (n){
+            result += (n % 10);
+            n /= 10;
+        }
+        return result;
+    }
+public:
+    int countLargestGroup(int n) {
+        vector<int> freq(40, 0);
+        int maxi = 0, count = 0;
+        for (int i = 1; i <= n; ++i){
+            int x = digitSum(i);
+            maxi = max(maxi, ++freq[x]);
+        }
+
+        for (const int& val : freq) count += val == maxi;
+        return count;
+    }
+};
