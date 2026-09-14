ff8a576083a162aa6d06fb4fba039e55d1118fd6
Immanuel Vivek Tirkey <63895010+DeStRoYeR-droid@users.noreply.github.com>
2026-09-14 09:17:10 +0530

    Create 1768-MergeStringsAlternately

---
+++ 1768-MergeStringsAlternately/1768-MergeStringsAlternately.cpp
@@ -0,0 +1,16 @@
+// Last updated: 14/09/2026, 09:17:10
+class Solution {
+public:
+    string mergeAlternately(string word1, string word2) {
+        string result = "";
+        int index_1 = 0, index_2 = 0;
+        bool turn = 1;
+        while (index_1 < word1.length() && index_2 < word2.length()){
+            result += (turn) ? word1[index_1++] : word2[index_2++];
+            turn = !turn;
+        }
+        while (index_1 < word1.length()) result += word1[index_1++];
+        while (index_2 < word2.length()) result += word2[index_2++];
+        return result;
+    }
+};
