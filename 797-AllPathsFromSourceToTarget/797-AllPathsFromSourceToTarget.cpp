42dafd6deb34f8962ba54f6dbddf5f697c0f3ecc
Immanuel Vivek Tirkey <63895010+DeStRoYeR-droid@users.noreply.github.com>
2026-09-14 09:35:14 +0530

    Create 797-AllPathsFromSourceToTarget

---
+++ 797-AllPathsFromSourceToTarget/797-AllPathsFromSourceToTarget.cpp
@@ -0,0 +1,32 @@
+// Last updated: 14/09/2026, 09:35:14
+class Solution {
+public:
+    vector<vector<int>> allPathsSourceTarget(vector<vector<int>>& graph) {
+        const int n = graph.size();
+        vector<vector<int>> result;
+        queue<pair<int, vector<int>>> q;
+        vector<int> cur;
+        q.push({0, cur});
+
+        vector<bool> visited(n, false);
+        
+        while (!q.empty()){
+            auto top = q.front(); q.pop();
+            int node = top.first;
+            vector<int> path = top.second;
+
+            if (visited[node]) continue;
+            path.push_back(node);
+            if (node == n - 1){
+                result.push_back(path);
+                continue;
+            }
+
+            for (int& neigh : graph[node]){
+                if (visited[neigh]) continue;
+                q.push({neigh, path});
+            }
+        }
+        return result;
+    }
+};
