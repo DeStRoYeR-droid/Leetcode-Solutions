5450db3159935a358defae0fc4771df1687e1037
Immanuel Vivek Tirkey <63895010+DeStRoYeR-droid@users.noreply.github.com>
2026-09-14 09:28:59 +0530

    Create 1078-OccurrencesAfterBigram

---
+++ 1078-OccurrencesAfterBigram/1078-OccurrencesAfterBigram.cpp
@@ -0,0 +1,16 @@
+// Last updated: 14/09/2026, 09:28:59
+class Solution {
+public:
+    vector<string> findOcurrences(string text, string first, string second) {
+        vector<string> words;
+        stringstream ss(text);
+        string word;
+        while (ss >> word) words.push_back(word);
+
+        vector<string> result;
+        for (int i = 0; i < words.size() - 2; ++i){
+            if (words[i] == first && words[i + 1] == second) result.push_back(words[i + 2]);
+        }
+        return result;
+    }
+};
