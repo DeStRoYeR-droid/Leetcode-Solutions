2da672544ac5baa553614bdcb6fcfb7ec005574b
Immanuel Vivek Tirkey <63895010+DeStRoYeR-droid@users.noreply.github.com>
2026-09-14 09:36:09 +0530

    Create 700-SearchInABinarySearchTree

---
+++ 700-SearchInABinarySearchTree/700-SearchInABinarySearchTree.cpp
@@ -0,0 +1,21 @@
+// Last updated: 14/09/2026, 09:36:09
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* searchBST(TreeNode* root, int val) {
+        if (!root) return nullptr;
+        if (root->val == val) return root;
+        else if (root->val > val) return searchBST(root->left, val);
+        else return searchBST(root->right, val);
+    }
+};
