98a5f694a739ebd9a458cc240b9a6ff08adddf07
Immanuel Vivek Tirkey <63895010+DeStRoYeR-droid@users.noreply.github.com>
2026-09-14 09:14:14 +0530

    Create 1967-NumberOfStringsThatAppearAsSubstringsInWord

---
+++ 1967-NumberOfStringsThatAppearAsSubstringsInWord/1967-NumberOfStringsThatAppearAsSubstringsInWord.cpp
@@ -0,0 +1,12 @@
+// Last updated: 14/09/2026, 09:14:14
+class Solution {
+public:
+    int numOfStrings(vector<string>& patterns, string word) {
+        string_view s(word);
+        int cnt=0;
+        for(string_view p: patterns){
+            cnt+=s.find(p)!=-1;
+        }
+        return cnt;
+    }
+};
