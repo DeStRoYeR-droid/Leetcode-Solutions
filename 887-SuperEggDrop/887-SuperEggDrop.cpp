c863ad754daefe4a351fa6f9f1cf148d61d69f2a
Immanuel Vivek Tirkey <63895010+DeStRoYeR-droid@users.noreply.github.com>
2026-09-14 09:32:49 +0530

    Create 887-SuperEggDrop

---
+++ 887-SuperEggDrop/887-SuperEggDrop.cpp
@@ -0,0 +1,22 @@
+// Last updated: 14/09/2026, 09:32:48
+class Solution {
+public:
+    int superEggDrop(int k, int n) {
+        vector<vector<int>> dp(n + 1, vector<int>(k + 1, 0));
+        for (int j = 1; j <= k; j++) {
+            dp[1][j] = 1;
+        }
+        for (int m = 1; m <= n; m++) {
+            dp[m][1] = m;
+        }
+        for (int moves = 2; moves <= n; moves++) {
+            for (int eggs = 2; eggs <= k; eggs++) {
+                dp[moves][eggs] = 1 + dp[moves - 1][eggs - 1] + dp[moves - 1][eggs];
+                if (dp[moves][eggs] >= n) {
+                    return moves;
+                }
+            }
+        }
+        return n;
+    }
+};
