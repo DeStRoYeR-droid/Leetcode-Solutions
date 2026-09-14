dd8a57f6428328aa9ab28b6c7c17b032a9ca1e7a
Immanuel Vivek Tirkey <63895010+DeStRoYeR-droid@users.noreply.github.com>
2026-09-14 09:17:13 +0530

    Create 1780-CheckIfNumberIsASumOfPowersOfThree

---
+++ 1780-CheckIfNumberIsASumOfPowersOfThree/1780-CheckIfNumberIsASumOfPowersOfThree.cpp
@@ -0,0 +1,11 @@
+// Last updated: 14/09/2026, 09:17:13
+class Solution {
+public:
+    bool checkPowersOfThree(int n) {
+        while (n != 0){
+            if (n % 3 == 2) return false;
+            n /= 3;
+        }
+        return true;
+    }
+};
