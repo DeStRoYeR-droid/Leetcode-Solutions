dbca83a1216bc1b5bf7fafdc65e9af5283653384
Immanuel Vivek Tirkey <63895010+DeStRoYeR-droid@users.noreply.github.com>
2026-09-14 09:14:00 +0530

    Create 1984-MinimumDifferenceBetweenHighestAndLowestOfKScores

---
+++ 1984-MinimumDifferenceBetweenHighestAndLowestOfKScores/1984-MinimumDifferenceBetweenHighestAndLowestOfKScores.cpp
@@ -0,0 +1,18 @@
+// Last updated: 14/09/2026, 09:14:00
+class Solution {
+public:
+    int minimumDifference(vector<int>& nums, int k) {
+        int n = nums.size();
+        if (k == 1) return 0;
+
+        sort(nums.begin(), nums.end());
+
+        int minDiff = INT_MAX;
+        for (int i = 0; i <= n - k; i++) {
+            int diff = nums[i + k - 1] - nums[i];
+            minDiff = min(minDiff, diff);
+        }
+
+        return minDiff;
+    }
+};
